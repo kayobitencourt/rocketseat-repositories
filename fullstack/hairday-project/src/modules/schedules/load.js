d61264be4a22194a20c622d56bdff9ccd498e464
Kayo Bitencourt <kayobitencourt.dev@gmail.com>
2025-03-22 17:24:15 -0300

    Buscando os horarios na api e renderizando em tela

--- fullstack/hairday-project/src/modules/schedules/load.js
+++ fullstack/hairday-project/src/modules/schedules/load.js
@@ -1,9 +1,18 @@
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+import { schedulesShow } from "./show.js";
 const selectedDate = document.getElementById("date");
 
-export function scheduleDay() {
+export async function scheduleDay() {
   //obtem a data do input
   const date = selectedDate.value;
 
+  //Busca na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+
+  //Exibe os agendamentos
+  schedulesShow({ dailySchedules });
+
+  //Renderiza as horas disponiveis
   hoursLoad({ date });
 }
--- fullstack/hairday-project/src/services/schedule-fetch-by-day.js
+++ fullstack/hairday-project/src/services/schedule-fetch-by-day.js
@@ -5,11 +5,12 @@ export async function scheduleFetchByDay({ date }) {
   try {
     const response = await fetch(`${apiConfig.baseURL}/schedules`);
     const data = await response.json();
-    const dailySchedules = data.filter((schedule) => {
-      dayjs(date).isSame(schedule.when, "day");
-    });
 
-    return dailySchedules();
+    const dailySchedules = await data.filter((schedule) =>
+      dayjs(date).isSame(schedule.when, "day")
+    );
+
+    return dailySchedules;
   } catch (error) {
     alert("Nao foi possivel buscar os dias");
     console.log(error);
